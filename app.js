58da31813f03687cda8399f9c2c3e1a0e34863b0
Max Anderson <omaxwellanderson@gmail.com>
2018-11-22 23:05:22 -0600

    got google image search working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const router = require('./routes/index');
 
 var app = express();
 
@@ -10,12 +11,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-console.log(path.join(__dirname, 'client'));
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'client')));
 
+app.use('/api', router);
+/*
 app.get('/api/helloworld', (req, res) => {
+	console.log("test");
 	res.send("Hello World!");
 });
+*/
 
 const port = process.env.PORT || '8080';
 app.listen(port);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.send("Error: please select an api method");
 });
+
+router.get('/movie/:movieName', function(req, res) {
+	let q = encodeURI(req.params.movieName + " movie poster");
+	let apiKey = "AIzaSyBUJxyACurBaMdBYq4IOkKBEJev3fPDTgA";
+	let cx = "013276211957766095160:aw87ssmotqe";
+	let url = "https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" +
+		cx + "&searchType=image&num=5&q=" + q;
+	fetch(url).then((res) => {
+		return res.json();
+	})
+	.then((json) => {
+		res.send(json);
+	});
+});
+
 
 module.exports = router;
