0718ab97df38169e7157e71559c7b78662bf3b5a
Max Anderson <omaxwellanderson@gmail.com>
2018-11-24 12:33:16 -0600

    Updated title css and font

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -4,7 +4,7 @@ class TitleBar extends React.Component {
 	render() {
 		return(
 			<div className="row">
-				<h3 className="col s6 offset-s3 center-align">Movie Night</h3>
+				<h2 className="col s6 offset-s3 center-align">Movie Night</h2>
 			</div>
 		)
 	}
