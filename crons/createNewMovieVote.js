f2a11be6481da933169d3467b4cad044498a0211
Max Anderson <omaxwellanderson@gmail.com>
2018-11-26 18:44:18 -0600

    Finished up cron

--- crons/createNewMovieVote.js
+++ crons/createNewMovieVote.js
@@ -1,15 +1,24 @@
 const apiKey = require('../apiKey');
+const fetch = require('node-fetch');
+
+console.log(apiKey);
 
 // make POST request to the backend
-fetch("http://45.79.19.55:8080/api/newMovieVote", {
+fetch("http://45.79.19.55:8080/api/newVote", {
 	method: "POST",
-	body: {
+	headers: {
+		"Content-Type": "application/json"
+	},
+	body: JSON.stringify({
 		apiKey: apiKey
-	}
-)
+	})
+})
 	.then((res) => {
 		return res.json();
 	})
 	.then((data) => {
 		console.log(data);
+	})
+	.catch((err) => {
+		console.log(err);
 	});
