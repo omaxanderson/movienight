7e1af21c9776cde7027502a9e44e57a654824677
O Max Anderson <anderso2@miamioh.edu>
2018-12-12 20:49:16 -0600

    added environment check before trying to configure https server

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ini = require('ini');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
 const router = require('./routes/index');
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourmovienight.com/privkey.pem', 'utf8');
-//const certificate = fs.readFileSync('/etc/letsencrypt/live/yourmovienight.com/cert.pem', 'utf8');
-const cert = fs.readFileSync('/etc/letsencrypt/live/yourmovienight.com/fullchain.pem', 'utf8');
+const config = ini.parse(fs.readFileSync('./environment.ini', 'utf8'));
 
-const credentials = {
-	key: privateKey,
-	cert: cert
-};
+if (config.env !== 'DEBUG') {
+	var privateKey = fs.readFileSync('/etc/letsencrypt/live/yourmovienight.com/privkey.pem', 'utf8');
+	//const certificate = fs.readFileSync('/etc/letsencrypt/live/yourmovienight.com/cert.pem', 'utf8');
+	var cert = fs.readFileSync('/etc/letsencrypt/live/yourmovienight.com/fullchain.pem', 'utf8');
+
+	var credentials = {
+		key: privateKey,
+		cert: cert
+	};
+}
 
 var app = express();
 
@@ -40,19 +45,14 @@ app.use('/api', router);
 
 
 // set to port 8080 to match proxy route
-//const port = process.env.PORT || '8080';
-//app.listen(port);
-//
-
-// @TODO I think what I need to do is serve the react app on a different port,
-// 	somehow configure 443 incoming requests to proxy to that port, and do
-// 	some other fucking magic
-
-// or i just need to make this a real fucking site instead of this garbage piece
-// of shit
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(8080, () => {
-	console.log('HTTPS server listening on port 8080');
-});
+if (config.env === 'DEBUG') {
+	const port = process.env.PORT || '8080';
+	app.listen(port);
+} else {
+	const httpsServer = https.createServer(credentials, app);
+	httpsServer.listen(8080, () => {
+		console.log('HTTPS server listening on port 8080');
+	});
+}
 
 module.exports = app;
