79bf7d23e0f5f12039b647b519cd45f4e35e581b
O Max Anderson <anderso2@miamioh.edu>
2018-12-15 02:07:20 -0600

    Insert new cookie into the database on successful login

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const dbconfig = require('../dbconfig');
 var mysql = require('mysql');
 const bcrypt = require('bcryptjs');
+const uniqid = require('uniqid');
+const md5 = require('md5');
 
 router.get('/', (req, res) => {
 	console.log('hit it');
@@ -13,9 +15,8 @@ router.post('/login', (req, res) => {
 	var connection = mysql.createConnection(dbconfig);
 	connection.connect();
 	const params = JSON.parse(req.body);
-	console.log(req.session);
 
-	const sql = `SELECT password 
+	const sql = `SELECT user_id, password 
 		FROM user
 		WHERE username = '${params.username}'`;
 
@@ -29,11 +30,31 @@ router.post('/login', (req, res) => {
 			res.send(JSON.stringify({status: 'error', message: 'Incorrect password'}));
 		} else {
 			// we're good, generate the cookie
+			const cookie = md5(params.username + Date.now());
 			
 			// save the cookie to the db
-			
-			// set that cookie!
-			res.send(JSON.stringify({status: 'success'}));
+			const insertSql = `REPLACE INTO session_cookie 
+				(user_id, cookie) VALUES (${rows[0]['user_id']}, '${cookie}')`;
+
+			connection.query(insertSql, (err, rows, fields) => {
+				console.log(rows.affectedRows);
+				if (err) {
+					// do some error handling
+					console.log(err);
+				} else if ([1,2].includes(rows.affectedRows)) {
+					// 1 if just insert, 2 if replacement
+					// set that cookie!
+					console.log('success!');
+					res.cookie('loginCookie', cookie, {
+						maxAge: 1000 * 60 * 60 * 24 * 7 * 2, // 2 weeks
+						httpOnly: true
+					});
+					res.send(JSON.stringify({status: 'success'}));
+				} else {
+					// something bad happened :(
+					res.send(JSON.stringify({status: 'error'}));
+				}
+			});
 		}
 	});
 });
